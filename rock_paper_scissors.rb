48e7d5165b6dffd49d0ff70d9fba1d67b9afc3ca
josedulanto <jose.dulanto@live.com>
2013-03-06 00:23:38 -0600

    Changed move from 'stone' to 'rock' and added a root path with simple game instructions

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,21 +1,25 @@
 require 'sinatra'
 
+get '/' do
+  '<h2>Let\'s play Rock, Paper, Scissors!</h2>To play write your move in the url path, example: <strong>http://localhost:4567/rock</strong> and if you\'re lucky you could beat the CPU!'
+end
+
 get '/:move' do
-	defeats = { scissors: :stone, stone: :paper, paper: :scissors }
+	defeats = { scissors: :rock, rock: :paper, paper: :scissors }
 	you = params[:move]
-	unless defeats.include?(you.to_sym)
-		msg = "Not a valid move!"
+	if !defeats.include?(you.to_sym)
+		msg = 'Not a valid move!'
 	else
 		ai = defeats.keys.sample
 
 		msg = if ai.to_s == you.to_s
-			"<h1>It's a tie!</h1>"
+			'<h1>It\'s a tie!</h1>'
 		elsif ai.to_s == defeats[you.to_sym].to_s
-			"<h1>You lost!</h1>"
+			'<h1>You lost!</h1>'
 		else
-			"<h1>You win!</h1>"
+			'<h1>You win!</h1>'
 		end
-		msg = msg + "<span style='font-size:120%; font-weight:bold'>#{ai}</span> (Computer)<br/> VS <br/><span style='font-size:120%; font-weight:bold'>#{you}</span> (You)"		
+		msg = msg + "<span style='font-size:120%; font-weight:bold'>#{ai}</span> (Computer)<br/> VS <br/><span style='font-size:120%; font-weight:bold'>#{you}</span> (You)"
 	end
 	msg
 end
